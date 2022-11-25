16758ee0fc90201eb0a2cc2f141b24551f575c8f
Kat Sukkau <Katsukkau@gmail.com>
2022-11-24 17:51:01 -0800

    working covnet model

---
+++ main.py
@@ -0,0 +1,126 @@
+import os
+import glob
+import keras
+import keras_video.utils
+# to use VideoFrameGenerator you must edit imports in the generator.py file, or it will not work
+# env/Lib/site-packages/keras_video/generator.py
+# from keras.preprocessing.image import ImageDataGenerator
+# from keras.utils import img_to_array
+from keras_video import VideoFrameGenerator
+from keras.layers import Conv2D, BatchNormalization, \
+    MaxPool2D, GlobalMaxPool2D
+from keras.layers import TimeDistributed, GRU, Dense, Dropout
+
+# use sub directories names as classes
+classes = [i.split(os.path.sep)[1] for i in glob.glob('videos/*')]
+classes.sort()
+# some global params
+SIZE = (112, 112)
+CHANNELS = 3
+NBFRAME = 5
+BS = 8
+# pattern to get videos and classes
+glob_pattern = 'videos/{classname}/*.mp4'
+# for data augmentation
+data_aug = keras.preprocessing.image.ImageDataGenerator(
+    zoom_range=.1,
+    horizontal_flip=True,
+    rotation_range=8,
+    width_shift_range=.2,
+    height_shift_range=.2)
+# Create video frame generator
+train = VideoFrameGenerator(
+    classes=classes,
+    glob_pattern=glob_pattern,
+    nb_frames=NBFRAME,
+    split=.33,
+    shuffle=True,
+    batch_size=BS,
+    target_shape=SIZE,
+    nb_channel=CHANNELS,
+    transformation=data_aug,
+    use_frame_cache=True)
+
+valid = train.get_validation_generator()
+
+keras_video.utils.show_sample(train)
+
+
+def build_convnet(shape=(112, 112, 3)):
+    momentum = .9
+    model = keras.Sequential()
+    model.add(Conv2D(64, (3, 3), input_shape=shape,
+                     padding='same', activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization(momentum=momentum))
+
+    model.add(MaxPool2D())
+
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization(momentum=momentum))
+
+    model.add(MaxPool2D())
+
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization(momentum=momentum))
+
+    model.add(MaxPool2D())
+
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(512, (3, 3), padding='same', activation='relu'))
+    model.add(BatchNormalization(momentum=momentum))
+
+    # flatten...
+    model.add(GlobalMaxPool2D())
+    return model
+
+
+def action_model(shape=(5, 112, 112, 3), nbout=3):
+    # Create our convnet with (112, 112, 3) input shape
+    convnet = build_convnet(shape[1:])
+
+    # then create our final model
+    model = keras.Sequential()
+    # add the convnet with (5, 112, 112, 3) shape
+    model.add(TimeDistributed(convnet, input_shape=shape))
+    # here, you can also use GRU or LSTM
+    model.add(GRU(64))
+    # and finally, we make a decision network
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(.5))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(nbout, activation='softmax'))
+    return model
+
+
+INSHAPE=(NBFRAME,) + SIZE + (CHANNELS,) # (5, 112, 112, 3)
+model = action_model(INSHAPE, len(classes))
+optimizer = keras.optimizers.Adam(0.001)
+model.compile(
+    optimizer,
+    'categorical_crossentropy',
+    metrics=['acc']
+    )
+
+EPOCHS=50
+# create a "chkp" directory before to run that
+# because ModelCheckpoint will write models inside
+callbacks = [
+    keras.callbacks.ReduceLROnPlateau(verbose=1),
+    keras.callbacks.ModelCheckpoint(
+        'chkp/weights.{epoch:02d}-{val_loss:.2f}.txt',
+        verbose=1),
+]
+model.fit_generator(
+    train,
+    validation_data=valid,
+    verbose=1,
+    epochs=EPOCHS,
+    callbacks=callbacks
+)
